25356cb6729e4ca12d46ab309d1bd052ba241092
Juan Urdaneta <jurdanetac10@gmail.com>
2026-03-12 19:54:45 -0400

    WIP: Rich copying of template to clipboard

--- frontend/src/components/CopyToClipboardButton.tsx
+++ frontend/src/components/CopyToClipboardButton.tsx
@@ -1,5 +1,30 @@
-const copyTextToClipboard = () => {
-  window.alert("TODO");
+const copyTextToClipboard = async () => {
+  const element = document.getElementById("templateContainer") as HTMLElement;
+
+  // const innerText = element.innerText
+  const innerHTML = element.innerHTML || ""; // fallback to empty string if element is null
+
+  // const blobText = new Blob([plainText], { type: "text/plain" });
+
+  const blobHtml = new Blob([innerHTML], { type: "text/html" });
+
+  const data = [
+    new ClipboardItem({
+      ["text/html"]: blobHtml,
+      // ["text/plain"]: blobText,
+    }),
+  ];
+
+  await navigator.clipboard.write(data);
+  window.alert("Formatted text copied! Now just paste it in your email.");
+
+  /*
+  const openEmail = () => {
+  const body = `Line 1\nLine 2\n\nBest regards, Your App`;
+  const encodedBody = encodeURIComponent(body);
+  window.location.href = `mailto:someone@example.com?subject=Hello&body=${encodedBody}`;
+};
+   */
 };
 
 const CopyToClipboardButton = () => {
