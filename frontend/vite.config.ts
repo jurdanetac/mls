f587f7818080db741919b7cf0076f7266713becf
Juan Urdaneta <jurdanetac10@gmail.com>
2026-03-19 18:58:30 -0400

    Add base to vite config deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // https://vite.dev/guide/static-deploy#github-pages/
+  base: "/mls/",
   plugins: [react()],
 });
