a341bf75000b07587a1e8c2cef028274ddc9a625
Juan Urdaneta <jurdanetac10@gmail.com>
2026-03-25 19:20:02 -0400

    Add prototype model

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class PropertyListing(models.Model):
+    class Status(models.TextChoices):
+        # Format: DB_VALUE = 'DB_VALUE', 'Human Readable Label'
+        ON_MARKET = "onMarket", "Active"
+        OFF_MARKET = "offMarket", "Off Market"
+        MEMBERS_ONLY_SHOW = "membersOnlyShow", "Members Only - Show"
+        MEMBERS_ONLY_DO_NOT_SHOW = "membersOnlyDoNotShow", "Members Only - Do Not Show"
+
+    status = models.CharField(
+        max_length=25, choices=Status.choices, help_text="Current listing status"
+    )
+
+    address = models.CharField(max_length=255)
+    bedrooms = models.PositiveIntegerField()
+    full_bathrooms = models.PositiveIntegerField()
+    half_bathrooms = models.PositiveIntegerField()
+    garage = models.PositiveIntegerField()
+    sqft = models.PositiveIntegerField()
+    sqft_lot = models.DecimalField(max_digits=12, decimal_places=2)
+    listing_price = models.DecimalField(max_digits=12, decimal_places=2)
+    age = models.PositiveIntegerField()
+    listing_agent = models.CharField(max_length=100)
+    listing_agent_office = models.CharField(max_length=150)
+    school_district = models.CharField(max_length=150)
+    arv = models.DecimalField(max_digits=12, decimal_places=2)
+
+    # Off-market Optionals
+    mls_number = models.CharField(max_length=50, null=True, blank=True)
+    dom = models.PositiveIntegerField(null=True, blank=True)
+    disclosures = models.TextField(null=True, blank=True)
+    open_house = models.CharField(max_length=255, null=True, blank=True)
+    private_notes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Property Listing"
+        verbose_name_plural = "Properties Listings"
+
+    def __str__(self):
+        return f"{self.address} - {self.get_status_display()}"
